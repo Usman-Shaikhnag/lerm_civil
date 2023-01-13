a3b765951a00f0fc0d373991885cb4fd59eebdf6
NagarajKalakutagi <ntkalakutagi@gamail.com>
2023-01-13 10:41:31 +0530

    sample entry

--- models/srf.py
+++ models/srf.py
@@ -1,8 +1,26 @@
 from odoo import api,fields,models
 
+class SampleEntry(models.Model):
+    _name = "lerm.sampleentry"
+    _description = "Lerm SRF Sample"
+
+    sample_id = fields.Char(string="Sample Id Mark")
+    brand = fields.Char(string="Brand")
+    size = fields.Char(string="Size")
+    grade = fields.Char(string="Grade")
+    sample_qty = fields.Char(string="Sample Qty")
+    sample_received_by = fields.Char(string="Sample Received By")
+    sample_received_date = fields.Date(string="Sample Received Date")
+    Sample_description = fields.Char(string="Sample Description")
+    reason_id = fields.Char(string="SSample Rejected / Reason")
+    date_of_casting = fields.Date(string="Date Of Casting")
+
+
+
 class LermSrf(models.Model):
     _name = "lerm.srf"
     _description = "Lerm SRF"
+    _inherit=['lerm.sampleentry']
 
     srf_no = fields.Char(string="SRF No")
     srf_date = fields.Date("SRF Date")
