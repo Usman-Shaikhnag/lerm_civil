4a1f51766e6f2a3af25c286b5fc55babb9389a04
Usman-Shaikhnag <ushaikhnag@gmail.com>
2023-01-16 13:35:34 +0000

    sample fixed

--- models/srf.py
+++ models/srf.py
@@ -13,8 +13,9 @@ class SampleEntry(models.Model):
     sample_received_by = fields.Char(string="Sample Received By")
     sample_received_date = fields.Date(string="Sample Received Date")
     Sample_description = fields.Char(string="Sample Description")
-    reason_id = fields.Char(string="SSample Rejected / Reason")
+    reason_id = fields.Char(string="Sample Rejected / Reason")
     date_of_casting = fields.Date(string="Date Of Casting")
+    sample_entry_id = fields.Many2one('lerm.srf',string="Sample id")
 
 
 class LermSrf(models.Model):
@@ -25,13 +26,13 @@ class LermSrf(models.Model):
     srf_date = fields.Date("SRF Date")
     customer = fields.Many2one('res.partner', string="Customer")
     billing_customer = fields.Many2one('res.partner', string="Billing Customer")
-    contact_person = fields.Char(string="Contact Person")
-    mobile_no = fields.Char(string="Person Mobile No")
-    email_id = fields.Char(string="Person Email id")
+    contact_person = fields.Char(string="Contact Person", compute='_compute_contact_person')
+    mobile_no = fields.Char(string="Person Mobile No",compute='_compute_mobile_no')
+    email_id = fields.Char(string="Person Email id", compute='_compute_email')
     client_ref = fields.Char(string="Client Reference")
     job_no = fields.Char(string="Job No")
     job_date = fields.Date(string="Job Date")
-    sample_entry = fields.One2many("lerm.sampleentry", "sample_id", string="Sample Entry")
+    sample_entry = fields.One2many("lerm.sampleentry", "sample_entry_id", string="Sample Entry")
 
     @api.onchange('customer')
     def _compute_contact_person(self):
